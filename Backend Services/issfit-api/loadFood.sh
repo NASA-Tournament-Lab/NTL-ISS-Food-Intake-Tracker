70b952ed1d39b473178922e81d47073db3efaf0c
Paulo Vitor Magacho da Silva <pvmagacho@gmail.com>
2017-02-05 00:44:30 -0200

    Update load food script

--- Backend Services/issfit-api/loadFood.sh
+++ Backend Services/issfit-api/loadFood.sh
@@ -40,7 +40,7 @@ fi
 
 if [ "${DROP}" -eq "1" ]; then
 psql "${CONN_STR}" << EOF
-  UPDATE food_product SET removed = TRUE;
+  UPDATE food_product SET removed = TRUE, modified_date = current_timestamp, synchronized = TRUE;
 EOF
 fi
 psql "${CONN_STR}" -f loadFood.sql > /dev/null
